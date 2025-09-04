94fa530e15b25412045c8f5237141e50b0e73d7a
jmarcelotse <jmarcelotse@hotmail.com>
2025-09-04 16:52:52 -0300

    adicionar sonarqube containet

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -10,7 +10,8 @@ def call (body) {
         echo "JOB_NAME=${env.JOB_NAME}"
 
          sonar-scanner -X \
-         -Dsonar.projectKey=${JOB_NAME} \
+         -Dsonar.token=${SONAR_LOGIN} \
+         -Dsonar.projectKey=${JOB_NAME}
         '''
     }
 
