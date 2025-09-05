d4be016c49d0805ce840d435e2854046dfd8c4c9
jmarcelotse <jmarcelotse@hotmail.com>
2025-09-05 10:53:41 -0300

    fix: adicionar SONAR_HOST_URL e parâmetros obrigatórios

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -8,10 +8,14 @@ def call (body) {
     container('sonar-scanner-cli') {
         sh '''
         echo "JOB_NAME= ${JOB_NAME}"
+        echo "SONAR_HOST_URL= ${SONAR_HOST_URL}"
 
          sonar-scanner -X \
+         -Dsonar.host.url=${SONAR_HOST_URL} \
          -Dsonar.token=${SONAR_TOKEN} \
-         -Dsonar.projectKey=${JOB_NAME%/*}-$GIT_BRANCH
+         -Dsonar.projectKey=${JOB_NAME%/*}-$GIT_BRANCH \
+         -Dsonar.sources=. \
+         -Dsonar.exclusions=venv/**,tests/**,.pytest_cache/**
         '''
     }
 
